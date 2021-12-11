e30d33558974e84f0f280ba0035d52728072ecf3
ponta <mponta2002@gmail.com>
2021-12-12 01:04:29 +0900

    ぽんたのコードだよ❤

---
+++ sample3.js
@@ -0,0 +1,11 @@
+
+let question_list= ["バナナ","リンゴ","メロン","パイナップル","イチゴ","レモン","スイカ","ブドウ","キウイ",]
+
+
+question_list.forEach(function(value,number){
+      console.log(value);
+})
+
+
+let ramdom = Math.floor(Math.random()*3);
+console.log(ramdom)
